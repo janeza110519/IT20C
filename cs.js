513bd0c9f5d3102301aa650d403649980a6a81ed
Janeza Malaya <janezamalaya4@gmail.com>
2025-11-17 08:29:35 +0800

    added if else statement

--- cs.js
+++ cs.js
@@ -6,3 +6,23 @@ function legalAge(age){
     }
 }
 legalAge(50);
+
+
+function computeGrade(grade){
+   if(grade >= 94.5){
+    console.log("1");
+   } else if (grade >= "91.5"){
+    console.log("1.25");
+   } else if (grade >= "89.5"){
+    console.log("1.75");
+   } else if (grade >= "86.5"){
+    console.log("1.75");
+   } else if (grade >= "84.5"){
+    console.log("2");
+   } else {
+    console.log("Not within the transmutation");
+   }
+
+}
+
+computeGrade(85);
